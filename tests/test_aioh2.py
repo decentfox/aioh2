9ee24ccaf39f123ae4180f8ab620df1e489be7e9
Fantix King <fantix.king@gmail.com>
2016-02-04 11:51:32 +0800

    Add request header test

--- tests/test_aioh2.py
+++ tests/test_aioh2.py
@@ -9,13 +9,12 @@ Tests for `aioh2` module.
 import unittest
 import uuid
 
-from h2.events import PingAcknowledged
+from h2.events import PingAcknowledged, RequestReceived
 
 from . import async_test, BaseTestCase
 
 
 class TestServer(BaseTestCase):
-    @async_test
     def test_connect(self):
         pass
 
@@ -26,6 +25,17 @@ class TestServer(BaseTestCase):
         events = yield from self._expect_events()
         self.assertIsInstance(events[0], PingAcknowledged)
         self.assertEqual(events[0].ping_data, opaque_data)
+
+    @async_test
+    def test_request_headers(self):
+        headers = [(':method', 'GET'), (':path', '/index.html')]
+        stream_id = self.conn.get_next_available_stream_id()
+        self.conn.send_headers(stream_id, headers)
+        yield from self._expect_events(0)
+        event = yield from self.server.events.get()
+        self.assertIsInstance(event, RequestReceived)
+        self.assertEqual(event.stream_id, stream_id)
+        self.assertEqual(event.headers, headers)
 
 
 if __name__ == '__main__':
