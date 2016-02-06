256863caab430122f9abe398c84df681d6b84566
Fantix King <fantix.king@gmail.com>
2016-02-06 18:29:51 +0800

    Example for basic usage

--- examples/core.py
+++ examples/core.py
@@ -1,36 +1,7 @@
-=====
-aioh2
-=====
+import asyncio
+import aioh2
 
-.. image:: https://img.shields.io/pypi/v/aioh2.svg
-        :target: https://pypi.python.org/pypi/aioh2
-
-.. image:: https://img.shields.io/travis/decentfox/aioh2.svg
-        :target: https://travis-ci.org/decentfox/aioh2
-
-.. image:: https://readthedocs.org/projects/aioh2/badge/?version=latest
-        :target: https://readthedocs.org/projects/aioh2/?badge=latest
-        :alt: Documentation Status
-
-
-HTTP/2 implementation with hyper-h2_ on Python 3 asyncio.
-
-* Free software: BSD license
-* Documentation: https://aioh2.readthedocs.org.
-
-Features
---------
-
-* Asynchronous HTTP/2 client and server
-* Managed flow control
-* More to come
-
-Example
--------
-
-
-.. code-block:: python
-
+async def example():
     # Server request handler
     async def on_connected(proto):
         while True:
@@ -81,12 +52,5 @@ Example
     trailers = await client.recv_trailers(stream_id)
     print(trailers)
 
+asyncio.get_event_loop().run_until_complete(example())
 
-Credits
--------
-
-This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
-
-.. _Cookiecutter: https://github.com/audreyr/cookiecutter
-.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
-.. _hyper-h2: https://github.com/python-hyper/hyper-h2
