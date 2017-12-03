b34482077de3d12200294c37b81aca8c2049ff90
Fantix King <fantix.king@gmail.com>
2017-12-03 17:14:44 +0800

    Bump version: 0.2.0 → 0.2.1

--- aioh2/__init__.py
+++ aioh2/__init__.py
@@ -6,4 +6,4 @@ from .protocol import *
 __all__ = exceptions.__all__ + helper.__all__ + protocol.__all__
 __author__ = 'Fantix King'
 __email__ = 'fantix.king@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
